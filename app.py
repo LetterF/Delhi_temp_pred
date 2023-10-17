681d2db5abd9331d608ec3a5a74a17b04b875cb9
FathanZ <78548764+LetterF@users.noreply.github.com>
2023-10-17 18:44:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,17 @@ import pickle
 import numpy as np
 app = Flask(__name__)
 
-model 
+model=pickle.load(open("predModel.pkl", "rb"))
+@app.route("/")
+
+def homePage():
+  return render_template("index.html")
+
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+  print(request.form)
+  int_features=[int(x) for x in request.form.values()]
+  final=[np.array(int_features)]
+  print(int_features)
+  print(final)
+  prediction=model.predict()
